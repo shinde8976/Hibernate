6dc4e0b9d7a31262a9b494a8475deb4615895393
ShindeAjay324 <shindeajay324@gmail.com>
2024-11-06 22:57:17 +0530

    Queue and stack

--- JavaPractice/src/InterviewQuestions.java
+++ JavaPractice/src/InterviewQuestions.java
@@ -1,14 +1,14 @@
 public class InterviewQuestions {
 
-    public boolean hasCycle(LL head){
-        LL fast = head;
-        LL slow = head;
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
 
-        while (fast != null && fast.next != null ){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
 
-            if (fast == slow){
+            if (fast == slow) {
                 return true;
             }
         }
@@ -26,4 +26,5 @@ public class InterviewQuestions {
             val = x;
             next = null;
         }
+    }
 }
--- JavaPractice/src/Main.java
+++ JavaPractice/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         list2.insertLast(56);
         list2.insertLast(70);
 
-         LL ans = LL.merge(list1,list2);
+        LL ans = LL.merge(list1, list2);
 
         ans.display();
 
@@ -19,140 +19,140 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    public int lengthCycle(ListNode head){
+    public int lengthCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
 
-        while(fast!=null && fast.next!=null){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
 
             if (fast == slow) {
                 ListNode temp = slow;
-                int length =0;
+                int length = 0;
 
                 do {
                     temp = temp.next;
                     length++;
-                }while(temp!=slow);
-                    return length;
+                } while (temp != slow);
+                return length;
             }
         }
         return 0;
     }
 
-    public ListNode detectCycle(ListNode head){
+    public ListNode detectCycle(ListNode head) {
         int length = 0;
         ListNode fast = head;
         ListNode slow = head;
 
-        while(fast!= null && fast.next!=null){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
 
-            if(fast == slow){
+            if (fast == slow) {
                 ListNode temp = slow;
 
-                do{
+                do {
                     temp = temp.next;
                     length++;
-                }while(temp != slow);
+                } while (temp != slow);
                 break;
             }
         }
 
         //Find the start Node
-        ListNode f= head;
-        ListNode s =head;
+        ListNode f = head;
+        ListNode s = head;
 
-        while(length>0){
+        while (length > 0) {
             s = s.next;
             length--;
         }
 
         //Keep Moving both forward and they will meet at cycle start
-        while(f!=s){
+        while (f != s) {
             f = f.next;
-            s= s.next;
-        }
-        return s;
-    }
-
-    public ListNode middleNode(ListNode Head){
-        ListNode s= head;
-        ListNode f = head;
-
-        while(f!=null && f.next!=null){
-            f= f.next.next;
             s = s.next;
         }
         return s;
     }
 
-    public boolean isHappy(int n){
+    public ListNode middleNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+
+        while (f != null && f.next != null) {
+            f = f.next.next;
+            s = s.next;
+        }
+        return s;
+    }
+
+    public boolean isHappy(int n) {
         int slow = n;
         int fast = n;
 
-        do{
+        do {
             slow = findsquare(slow);
             fast = findsquare(findsquare(fast));
-        }while(slow!=fast);
+        } while (slow != fast);
 
-        if(slow == 1){
+        if (slow == 1) {
             return true;
         }
         return false;
     }
 
-    private int findsquare(int number){
+    private int findsquare(int number) {
         int ans = 0;
-        while(number>0){
-            int rem = number%10;
-            ans += rem*rem;
+        while (number > 0) {
+            int rem = number % 10;
+            ans += rem * rem;
             number /= 10;
         }
         return ans;
     }
 
     //Merge Sort
-    public ListNode sortList(ListNode head){
-        if(head == null || head.next == null){
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
             return head;
         }
 
-        ListNode mis = getMid(head);
+        ListNode mid = getMid(head);
         ListNode left = sortList(head);
         ListNode right = sortList(mid);
 
         return merge(left, right);
     }
 
-    ListNode merge(ListNode list1, ListNode list2){
+    ListNode merge(ListNode list1, ListNode list2) {
 
         ListNode dummyHead = new ListNode();
         ListNode tail = dummyHead;
 
-        while(list1 != null && list2 !=null){
-            if(list1.val <list2.val){
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
                 tail.next = list1;
                 list1 = list1.next;
                 tail = tail.next;
-            }else{
+            } else {
                 tail.next = list2;
                 list2 = list2.next;
-                tail =tail.next;
+                tail = tail.next;
             }
         }
 
-        tail.next = (list1!=null) ? list1:list2;
+        tail.next = (list1 != null) ? list1 : list2;
         return dummyHead.next;
     }
 
-    ListNode getMid(ListNode head){
+    ListNode getMid(ListNode head) {
         ListNode midPrev = null;
 
-        while(head!= null && head.next != null){
-            midPrev = (midPrev == null)? head:midPrev.next;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
             head = head.next.next;
         }
         ListNode mid = midPrev.next;
@@ -171,4 +171,5 @@ public class Main {
             val = x;
             next = null;
         }
+    }
 }
